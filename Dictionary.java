e935449d9bbf6028b97216184abab30ae24fe39f
anhquocuet <anhquoc10.tvt@gmail.com>
2023-10-14 17:09:54 +0700

    Dictionary cải tiến 14/10

---
+++ Dictionary.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+  private ArrayList<Word> wordList = new ArrayList<>() ;
+  public void addWord(Word word) {
+    wordList.add(word);
+  }
+  public ArrayList<Word> getWordList() {
+    return wordList;
+  }
+}
---
+++ DictionaryManagement.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DictionaryManagement {
+  public void insertFromCommandline() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhập số lượng từ:\n");
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+  }
+}
