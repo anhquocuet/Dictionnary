1e1e7e256f773fc12eb78fda76d2ccfdeb3b081f
anhquocuet <anhquoc10.tvt@gmail.com>
2023-11-13 12:32:47 +0700

    13/11/2023

--- Dictionary.java
+++ Dictionary.java
@@ -1,27 +1,54 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Dictionary {
+    protected static ArrayList<Word> words;
 
-  public ArrayList<Word> words;
+    public Dictionary() {
+        words = new ArrayList<>();
+    }
 
-  /**
-   * khoitao.
-   */
-  public Dictionary() {
-    words = new ArrayList<Word>();
-  }
+    public static void sortWordList(ArrayList<Word> wordArrayList) {
+        Collections.sort(wordArrayList, new Comparator<Word>() {
+            public int compare(Word word1, Word word2) {
+                return word1.getWord_target().compareTo(word2.getWord_target());
+            }
+        });
+    }
 
-  /**
-   * addword.
-   */
-  public void addNewords(Word word) {
-    words.add(word);
-  }
+    public static void addWord(Word word) {
+        int index = Collections.binarySearch(words,
+                word,
+                Comparator.comparing(Word::getWord_target));
+        if (index < 0) {
+            index = -(index + 1);
+            words.add(index, word);
+            sortWordList(words);
+        } else System.out.println("Vocab has already existed!!");
+    }
 
-  /**
-   * getter.
-   */
-  public ArrayList<Word> getWords() {
-    return words;
-  }
+    public void DisplayAllWord() {
+        System.out.println("+------+----------------------+---------------------------+");
+        System.out.println("| NO   | English              | Vietnamese                |");
+        System.out.println("+------+----------------------+---------------------------+");
+        for (int i = 0; i < words.size(); ++i) {
+            String format = String.format("| %-4d | %-20s | %-25s |",
+                    (i + 1), words.get(i).getWord_target(), words.get(i).getWord_explain());
+            String line = String.format("%4s", "").replace(' ', '-');
+            String line1 = String.format("%20s", "").replace(' ', '-');
+            String line2 = String.format("%25s", "").replace(' ', '-');
+            String formatDown = "+-" + line + "-+-" + line1 + "-+-" + line2 + "-+";
+            System.out.println(format);
+            System.out.println(formatDown);
+        }
+    }
+
+    public ArrayList<Word> getWords() {
+        return words;
+    }
+
+    public void setWords(ArrayList<Word> words) {
+        this.words = words;
+    }
 }
--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,31 +1,16 @@
-import java.util.Collections;
-import java.util.Comparator;
+import java.io.IOException;
 
-public class DictionaryCommandline {
-  public DictionaryManagement dManagement;
-
-  public DictionaryCommandline(DictionaryManagement dictionaryManagement) {
-    this.dManagement = dictionaryManagement;
-  }
-
-  public void showAllWords() {
-    /** sapxep word theo alphabet. */
-    Collections.sort(dManagement.dictionary.words, new Comparator<Word>() {
-      @Override
-      public int compare(Word word1, Word word2) {
-        return word1.getWord_target().compareTo(word2.getWord_explain());
-      }
-    });
-    /** in ra. */
-    System.out.printf("%-5s |  %-20s |  %-20s%n", "No", "English", "Vietnamese");
-    for (int i = 0; i < dManagement.dictionary.words.size(); i++) {
-      Word word = dManagement.dictionary.words.get(i);
-      System.out.printf("%-5d |  %-20s |  %-20s%n", i + 1, word.getWord_target(), word.getWord_explain());
+public class DictionaryCommandline extends DictionaryManagement{
+    //Func
+    public void showAllWords(){
+        DisplayAllWord();
     }
-  }
-
-  public void dictionaryBasic() {
-    dManagement.insertFromCommandline();
-    showAllWords();
-  }
+    public void dictionaryBasic() {
+        DictionaryManagement.insertFromCommandline();
+        showAllWords();
+    }
+    public static void main(String[] args) {
+        DictionaryCommandline dictionaryCommandLine = new DictionaryCommandline();
+        dictionaryCommandLine.dictionaryBasic();
+    }
 }
