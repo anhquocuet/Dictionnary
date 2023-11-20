c1452cf2c6f1343d97bc185a4343c793f69c846f
anhquocuet <anhquoc10.tvt@gmail.com>
2023-11-20 19:52:41 +0700

    20/11

--- Dictionary.java
+++ Dictionary.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import static java.util.Collections.binarySearch;
+
 public class Dictionary {
     protected static ArrayList<Word> words;
 
@@ -17,30 +19,69 @@ public class Dictionary {
         });
     }
 
-    public static void addWord(Word word) {
-        int index = Collections.binarySearch(words,
-                word,
-                Comparator.comparing(Word::getWord_target));
-        if (index < 0) {
-            index = -(index + 1);
-            words.add(index, word);
-            sortWordList(words);
-        } else System.out.println("Vocab has already existed!!");
+    public int binarySearchWord(String word) {
+        int l = 0;
+        int r = words.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int result = words.get(mid).getWord_target().compareTo(word);
+            if (result == 0) {
+                return mid;
+            } else if (result < 0) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
     }
 
+    public static void addWord(Word word) {
+        int x = binarySearch(words,
+                word,
+                Comparator.comparing(Word::getWord_target));
+        if (x < 0) {
+            x = -(x + 1);
+            words.add(x, word);
+            sortWordList(words);
+        } else System.out.println("Từ vựng đã tồn tại!!\nHãy chọn chức năng sửa đổi...\n");
+    }
+
+    public String searchWord(String englishWord) {
+        int x = binarySearchWord(englishWord);
+        if (x >= 0) {
+            return "Giải nghĩa Tiếng Việt: " + words.get(x).getWord_explain()+ "\n";
+        }
+        return "Không tìm thấy từ vựng!";
+    }
+
+    public void updateWord(String word_target, String update_word_explain) {
+        for (int i = 0; i<words.size(); i++){
+            if (words.get(i).getWord_target().equals(word_target)) {
+                words.get(i).setWord_explain(update_word_explain);
+                System.out.println("Sửa thành công!\n");
+                return;
+            }
+        }
+        System.out.println("Không tìm thấy từ!\n");
+    }
+    public void removeWord(String englishWord) {
+        int x = binarySearchWord(englishWord);
+        if (x >= 0) {
+            words.remove(x);
+            System.out.println("Xoá thành công!\n");
+        } else {
+            System.out.println("Không tìm thấy từ\n");
+        }
+    }
     public void DisplayAllWord() {
-        System.out.println("+------+----------------------+---------------------------+");
-        System.out.println("| NO   | English              | Vietnamese                |");
-        System.out.println("+------+----------------------+---------------------------+");
+        System.out.printf("%-5s |  %-20s |  %-20s%n", "No", "English", "Vietnamese");
         for (int i = 0; i < words.size(); ++i) {
-            String format = String.format("| %-4d | %-20s | %-25s |",
+            String s = String.format("%-5d |  %-20s |  %-20s%n",
                     (i + 1), words.get(i).getWord_target(), words.get(i).getWord_explain());
-            String line = String.format("%4s", "").replace(' ', '-');
-            String line1 = String.format("%20s", "").replace(' ', '-');
-            String line2 = String.format("%25s", "").replace(' ', '-');
-            String formatDown = "+-" + line + "-+-" + line1 + "-+-" + line2 + "-+";
-            System.out.println(format);
-            System.out.println(formatDown);
+            System.out.println(s);
         }
     }
 
