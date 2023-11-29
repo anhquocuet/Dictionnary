6694a6622e1b6d94fdbc688349fa20310b1a86b4
anhquocuet <anhquoc10.tvt@gmail.com>
2023-11-29 22:22:40 +0700

    29/11

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Dictionary dictionary = new Dictionary();
-    DictionaryManagement dM = new DictionaryManagement(dictionary);
-    DictionaryCommandline dCommandLine = new DictionaryCommandline(dM);
-
-    dCommandLine.dictionaryBasic();
-  }
-}
